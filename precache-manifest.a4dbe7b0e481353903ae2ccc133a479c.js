af17f461531dd4b95e29997963daf88774c5a595
huizhuz <huizhuz@outlook.com>
2019-10-17 15:02:03 -0400

    Updates

---
+++ precache-manifest.a4dbe7b0e481353903ae2ccc133a479c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1541c89b42513d6c65857ccbf144ad7c",
+    "url": "/vocabuilder/index.html"
+  },
+  {
+    "revision": "c3df50173ea4a528eccc",
+    "url": "/vocabuilder/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "d49716e7d05ab95dcaaa",
+    "url": "/vocabuilder/static/js/2.3ad60d84.chunk.js"
+  },
+  {
+    "revision": "c3df50173ea4a528eccc",
+    "url": "/vocabuilder/static/js/main.ed38b07b.chunk.js"
+  },
+  {
+    "revision": "3398a9bb17e7187dd894",
+    "url": "/vocabuilder/static/js/runtime-main.3f2d9695.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/vocabuilder/static/media/logo.25bf045c.svg"
+  }
+]);

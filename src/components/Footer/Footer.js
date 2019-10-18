8ed7e1fcd072470bf2c7f4f351223a26abaebfb1
huizhuz <huizhuz@outlook.com>
2019-10-18 16:30:32 -0400

    Added font awesome icon

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import styles from './Footer.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCodeBranch } from '@fortawesome/free-solid-svg-icons'
+
+const github = <FontAwesomeIcon icon={faCodeBranch} />
 
 const Footer = () => {
     return (
         <div className={styles.Footer}>
             <div>
                 <p>Created with ReactJS</p>
+                <a target="_blank" href="https://github.com/huizhuz/vocabuilder/tree/master"><p>See the code on my  {github}  page</p></a>
                 <p>&copy; Huizhu Zhang - October 2019</p>
             </div>
 

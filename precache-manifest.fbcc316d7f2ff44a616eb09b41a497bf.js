4941fc59f248b0b7ff3ab54c7cf64d98ace3a806
huizhuz <huizhuz@outlook.com>
2019-10-17 20:34:02 -0400

    Updates

---
+++ precache-manifest.fbcc316d7f2ff44a616eb09b41a497bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6b09b188e1f4915cb7a9bf5800f4a69",
+    "url": "/vocabuilder/index.html"
+  },
+  {
+    "revision": "a478ea4bf9a20f2efac4",
+    "url": "/vocabuilder/static/css/main.d1a1a6a4.chunk.css"
+  },
+  {
+    "revision": "0a30eea45d26a1e80140",
+    "url": "/vocabuilder/static/js/2.c21903ce.chunk.js"
+  },
+  {
+    "revision": "a478ea4bf9a20f2efac4",
+    "url": "/vocabuilder/static/js/main.e992cc6b.chunk.js"
+  },
+  {
+    "revision": "3398a9bb17e7187dd894",
+    "url": "/vocabuilder/static/js/runtime-main.3f2d9695.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vocabuilder/precache-manifest.a4dbe7b0e481353903ae2ccc133a479c.js"
+  "/vocabuilder/precache-manifest.fbcc316d7f2ff44a616eb09b41a497bf.js"
 );
 
 self.addEventListener('message', (event) => {

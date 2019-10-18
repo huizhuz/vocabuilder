fa3694a844c384a090a801015a8ddfe5d9cdf30d
huizhuz <huizhuz@outlook.com>
2019-10-18 15:58:44 -0400

    Finished OnePhraseADay page. Added data only from 18 to 31 of October. Not yet connected to firebase

--- src/containers/OnePhraseADay/OnePhraseADay.js
+++ src/containers/OnePhraseADay/OnePhraseADay.js
@@ -1,14 +1,43 @@
 import React, { Component } from 'react';
 import styles from './OnePhraseADay.module.css'
+import img6 from '../../assets/img/img7.jpeg'
 
+import phrases from './phrases'
+
+const monthNames = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+];
+const date = new Date();
+const month = monthNames[date.getMonth()];
+const day = date.getDate();
+const year = date.getFullYear();
 
 export class OnePhraseADay extends Component {
     render() {
-        return (
-            <div className={styles.OnePhraseADay}>
-                <p>phrase page</p>
-            </div>
-        );
+        let phrase = {phrase: 'Something went wrong', definition: 'Come back later!'}
+        for (let i = 1; i < phrases.length; i++){
+            if(phrases[i].day == day){
+                phrase.phrase = phrases[i].phrase;
+                phrase.definition = phrases[i].definition;
+            }
+        }
+            return (
+                <div className={styles.OnePhraseADay}>
+                    <h3>Phrase of The Day - {month} {day}, {year}</h3>
+                    <div
+                        className={styles.Phrase}
+                        style={{
+                            background: `linear-gradient(#f0e6e622, #f0e6e611), url(${img6}) no-repeat center`,
+                            backgroundSize: 'cover'
+
+                        }}>
+                        <h4>{phrase.phrase}</h4>
+                        <h5>{phrase.definition}</h5>
+                    </div>
+                    <p>Resources are found <a href="https://www.kaplaninternational.com/blog/halloween-idioms">here</a> and <a href="https://blog.esllibrary.com/2015/10/27/10-halloween-idioms-expressions/">here</a>!</p>
+                </div>
+
+            );
     }
 }
 
---
+++ src/containers/OnePhraseADay/phrases.js
@@ -0,0 +1,84 @@
+const phrases=[
+    {
+        day: '16',
+        phrase: 'Ghost town',
+        definition: 'An area that’s empty and devoid of life or activity.'
+    },
+    {
+        day: '17',
+        phrase: 'Witching hour',
+        definition: 'Midnight'
+    },
+    {
+        day: '18',
+        phrase: 'Blood run cold',
+        definition: 'This means you are so frightened that you can feel it all over.'
+    },
+    {
+        day: '19',
+        phrase: 'Play devil\'s advocate',
+        definition: 'Someone who plays "devil\'s advocate" is a person who argues an unpopular opinion for the sake of the argument.'
+    },
+    {
+        day: '20',
+        phrase: 'Dig one\'s grave',
+        definition: 'Do something silly or unwise which will cause you trouble.'
+    },
+    {
+        day: '21',
+        phrase: 'In cold blood',
+        definition: 'To do something "in cold blood" is to act in a purposefully ruthful manner.'
+    },
+    {
+        day: '22',
+        phrase: 'Like a bat out of hell',
+        definition: 'This term is used to describe something that happens very quickly or suddenly.'
+    },
+    {
+        day: '23',
+        phrase: 'Over my dead body',
+        definition: 'This is used when you refuse to let someone do something.'
+    },
+    {
+        day: '24',
+        phrase: 'Skeletons in the closet',
+        definition: 'Someone with "skeletons in their closet" is someone who has something they don\'t want to tell you about.'
+    },
+    {
+        day: '25',
+        phrase: 'Skeleton staff',
+        definition: 'A project that requires "skeleton staff" is one that needs the smallest possible number of people to be completed.'
+    },
+    {
+        day: '26',
+        phrase: 'Stab in the back',
+        definition: 'To betray someone.'
+    },
+    {
+        day: '27',
+        phrase: 'Witch hunt',
+        definition: 'To try and punish people with unpopular opinions.'
+    },
+    {
+        day: '28',
+        phrase: 'Scared stiff',
+        definition: 'Very frightened (so scared that a person can\'t move).'
+    },
+    {
+        day: '29',
+        phrase: 'Death trap',
+        definition: 'A building or other structure that is dangerous.'
+    },
+    {
+        day: '30',
+        phrase: 'Come back to haunt someone',
+        definition: 'To make a mistake that will affect a person later on.'
+    },
+    {
+        day: '31',
+        phrase: 'Make one\'s blood boil',
+        definition: 'To cause a person to be very angry.'
+    },
+]
+
+export default phrases

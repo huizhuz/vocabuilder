a9f0544a4c4c0ae21e08e99cbe655b78e1c8a4af
huizhuz <huizhuz@outlook.com>
2019-10-17 22:02:44 -0400

    Finished home page. Added images and routes. Minor style changes

--- src/components/Content/ContentCard/ContentCard.js
+++ src/components/Content/ContentCard/ContentCard.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom'
 import styles from './ContentCard.module.css'
+import linkStyles from '../../Header/Navigation/Navigation.module.css'
 
-const ContentCard = (props) => {
-    return (
-        <div className={styles.ContentCard}>
-            <p>{props.title}</p>
-        </div>
-    );
+class ContentCard extends Component {
+    render() {
+        return (
+            <div 
+                className={styles.ContentCard} 
+                onClick={() => this.props.history.push(this.props.route)}
+                style={{  
+                    background: `linear-gradient(#f3df9c85, #d6818185), url(${this.props.img}) no-repeat center`,
+                    backgroundSize: 'cover'
+                  }}>
+                <div className={linkStyles.Link}>{this.props.title}</div>
+            </div>
+        );
+    }
 }
 
-export default ContentCard;
+export default withRouter(ContentCard);
--- src/components/Content/ContentList.js
+++ src/components/Content/ContentList.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import ContentCard from './ContentCard/ContentCard';
-import styles from './ContentList.module.css';
+import React from 'react'
+import ContentCard from './ContentCard/ContentCard'
+import styles from './ContentList.module.css'
+import img1 from '../../assets/img/img1.jpeg'
+import img2 from '../../assets/img/img2.jpeg'
+import img3 from '../../assets/img/img3.jpeg'
 
 const ContentList = () => {
     const titles = [
-        'One Phrase A Day',
-        'Test Your Vocab Knowledge!',
-        'Contact The Creator'
+        {id: 0, title: 'One Phrase A Day', route: '/one-phrase-a-day', img: img1},
+        {id: 1, title: 'Test Your Vocabulary!', route: 'vocab-quiz', img: img2},
+        {id: 2, title: 'Contact The Creator', route: 'contact', img: img3}
     ];
     return (
         <div className={styles.ContentList}>
-            <p>Choose from below</p>
             {
-                titles.map(title => {
-                    return <ContentCard title={title} key={title}></ContentCard>
+                titles.map(eachTitle => {
+                    return <ContentCard title={eachTitle.title} route={eachTitle.route} img={eachTitle.img} key={eachTitle.id}></ContentCard>
                 })
             }
         </div>
@@ -21,3 +23,4 @@ const ContentList = () => {
 }
 
 export default ContentList;
+
